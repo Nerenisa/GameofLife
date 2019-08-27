4ccf21911968a177941db43c7350e9870f73e851
Alexandra Galdina <nerenisa@gmail.com>
2019-08-27 14:12:11 +0300

    class universe

--- gof.py
+++ gof.py
@@ -3,6 +3,19 @@
 
 
 import sys
+import os 
+import time
+
+#%%
+class universe(object):
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h 
+        self.content = {}
+        for y in range(self.h):
+            for x in range(self.w):
+                self.contents = [x, y] = 0
 
 
-
+#%%
