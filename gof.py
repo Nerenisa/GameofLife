ddd9b10067e5c8df0ed1a144fa9b61e192e12ec9
Alexandra Galdina <nerenisa@gmail.com>
2019-09-02 14:53:17 +0300

    --

--- gof.py
+++ gof.py
@@ -6,27 +6,58 @@ import sys
 import os 
 import time
 
-#%%
-class universe(object):
+
+class Universe(object):
 
     def __init__(self, w, h):
         self.w = w
         self.h = h 
-        self.content = {}
+        self.contents = {}
         for y in range(self.h):
             for x in range(self.w):
-                self.content = [x, y] = 0
-
+                self.contents = [x, y] = 0
+        self.toString = ""
+        for y in range(self):
+            for x in range(self.w):
+                c = self.contents = [x, y]
+                if c == 0:
+                    self.toString += " "
+                elif c == 1:
+                    self.toString += u"\u2588"
+            self.toString += "\n"        
+                
     def randomize(self):
+        out = {}
         for y in range(self.h):
             for x in range(self.w):
                 c = random.randint(0, 30)
-                if == 0:
+                if c == 0:
+                    out[x, y] = 1
+                else:
+                    out[x, y] = 0
+        self.contents = out
+    
+
+    def getNeighbours(self):
+        pass
+
+
+try:
+    width = int(sys.argv[1])
+except:
+    width = 100
+try:
+    height = int(sys.argv[2])
+except:
+    height = 12
+
+
+universe = Universe(width, height)
+universe.randomize()
+
+while True:
+    
 
 
 
 
-
-
-
-#%%
